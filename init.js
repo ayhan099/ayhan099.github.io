f1affb85b7c310a9443294ab52b99e2c64f8a8a9
ayhan099 <151830649+ayhan099@users.noreply.github.com>
2026-04-02 14:44:51 +0200

    55% klar

--- init.js
+++ init.js
@@ -1,17 +1,51 @@
-document.addEventListener('DOMContentLoaded', function () {
-        const nav = `
-        <nav>
+document.addEventListener("DOMContentLoaded", function () {
+    const nav = `
+    <nav>
         <ul class="navbar">
-        <li><a href="index.html">Hem</a></li>
-        <li><a href="boka.html">Boka</a></li>
-        <li><a href="galleri.html">Galleri</a></li>
-        <li><a href="priser.html">Priser</a></li>
-        <li><a href="about.html">Om oss</a></li>
-        <li><a href="recen.html">Recensioner</a></li>
+            <li><a href="index.html">Hem</a></li>
+            <li><a href="boka.html">Boka</a></li>
+            <li><a href="galleri.html">Galleri</a></li>
+            <li><a href="priser.html">Priser</a></li>
+            <li><a href="about.html">Om oss</a></li>
+            <li><a href="recen.html">Recensioner</a></li>
         </ul>
     </nav>
     `;
 
-document.getElementById("navbar").innerHTML = nav;
+    const navbarDiv = document.getElementById("navbar");
+    if (navbarDiv) {
+        navbarDiv.innerHTML = nav;
+    }
+
+    const footer = `
+    <footer class="footer">
+
+    <div class="footer-top">
+        <h2>Mrs Pärson Hotel</h2>
+    </div>
+
+    <div class="footer-bottom">
+        <p>© 2026 Mrs Pärson Hotel</p>
+        <div class="footer-links">
+            <a href="integritet.html">Integritetspolicy</a>
+            <a href="cookies.html">Cookies</a>
+        </div>
+    </div>
+
+    </footer>
+    `;
+
+    const footerDiv = document.getElementById("footer");
+    if (footerDiv) {
+        footerDiv.innerHTML = footer;
+    }
 
 });
+
+function openImage(img){
+    const modalImg = document.getElementById("modalImage");
+    modalImg.src = img.src;
+
+    const modal = new bootstrap.Modal(document.getElementById('imageModal'));
+    modal.show();
+}
